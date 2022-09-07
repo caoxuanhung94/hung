7d64d3b3e30fd8035d7d5f213b97cd083e673ca4
caoxuanhung94 <xuanhungbka20120446@gmail.com>
2022-09-07 21:21:36 +0700

    Tao code moi

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<link href="{{ asset('Css/style.css') }}" rel="stylesheet">
+<div id="header" >
+    <h1>header</h1>
+</div>
+<div id="body" >
+    <h1>body</h1>
+</div>
+<div id="footer" >
+    <h1>footer</h1>
+</div>
